889a63ddeb342847b63915c9f2e1417693abd972
Joel-K-Muraguri <joelkmuraguri21@gmail.com>
2022-06-06 11:41:25 +0300

    Initial-Movie

--- app/src/main/java/com/joel/list/utils/ApiConstant.kt
+++ app/src/main/java/com/joel/list/utils/ApiConstant.kt
@@ -1,6 +1,11 @@
 package com.joel.list.utils
 
 object ApiConstant {
+    /*
+    const val CAR_BASE_URL = "https://private-35a53a-carsapi1.apiary-mock.com/"
+    const val CAR_END_POINT = "cars"
+     */
+
     const val BASE_URL = "https://howtodoandroid.com/apis/"
     const val END_POINT = "movielist.json"
 }
